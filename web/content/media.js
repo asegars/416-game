3db3db52166fdb2fee8f4a225afd07798e1f0245
Luke Segars <luke@luke-desktop.(none)>
2009-12-02 17:34:24 -0500

    Added FLV gameplay video to bottom of screenshot page

--- web/content/media.js
+++ web/content/media.js
@@ -2,7 +2,7 @@ var c_media = "<h2>Screenshots & Videos</h2> \
 	<h3>Screenshots</h3> \
  	<div style='border-width: 0px;' id='images'> \
 		<p>Some action shots from inside the game:</p> \
-		<img src='media/ss1.jpg' style='width: 480px; height: 360px; border: 1px solid black;' /> \
+		<img onload='loadPlayer();' src='media/ss1.jpg' style='width: 480px; height: 360px; border: 1px solid black;' /> \
 		<p style='width: 100%; text-align: center;'>Screenshot 1: A player vaporizes a monster with an oversized laser gun.</p> \
 		<img src='media/ss2.jpg' style='width: 480px; height: 360px; border: 1px solid black;' /> \
 		<p style='width: 100%; text-align: center;'>Screenshot 2: There's no hiding when the waters start rising.</p> \
@@ -13,4 +13,6 @@ var c_media = "<h2>Screenshots & Videos</h2> \
 	</div> \
 	<h3>Gameplay Videos</h3> \
 		<p>The well-known and widely cherished gameplay video:</p> \
-	";
+ 		<div id='v170' style='border-width: 0px; text-align: center;> \
+			<a href='http://www.macromedia.com/go/getflashplayer'>Get the Flash Player</a> to see this video. \
+		</div>";
